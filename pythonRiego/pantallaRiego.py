8b8d0211c84808eee57e09127dcb92852a22f985
LAPTOP-N5O8LJNK\yarvi <yarvis42@gmail.com>
2024-05-09 11:33:47 -0700

    Agregar funciones y señales a las páginas de python y html

--- pythonRiego/pantallaRiego.py
+++ pythonRiego/pantallaRiego.py
@@ -27,6 +27,10 @@ class VisualizacionDatosRiego(tk.Toplevel):
         nivel_agua = 90
         humedad_tierra = 70
         hora_ultimo_riego = "10:30:45"
+        estado_valvula = "Abierta"
+
+        label = tk.Label(self, text="Consultar datos del riego", font=("Arial", 14))
+        label.pack(pady=20)
 
         label_nivel_agua = tk.Label(self, text=f"Nivel de agua: {nivel_agua}%")
         label_nivel_agua.pack()
@@ -37,14 +41,14 @@ class VisualizacionDatosRiego(tk.Toplevel):
         label_hora_ultimo_riego = tk.Label(self, text=f"Hora último riego: {hora_ultimo_riego}")
         label_hora_ultimo_riego.pack()
 
+        label_estado_valvula = tk.Label(self, text=f"Estado válvula: {estado_valvula}")
+        label_estado_valvula.pack()
+
         frame_botones = tk.Frame(self)
         frame_botones.pack(pady=20)
 
         button_atras = tk.Button(frame_botones, text="Atrás", command=self.volverAConsultar)
         button_atras.pack(side=tk.LEFT, padx=10)
-
-        button_adelante = tk.Button(frame_botones, text="Adelante", state=tk.DISABLED)
-        button_adelante.pack(side=tk.LEFT, padx=10)
 
     def volverAConsultar(self):
         self.withdraw()  # Ocultar esta ventana
